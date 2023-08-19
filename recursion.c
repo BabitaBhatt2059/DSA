79f080abe01e5fae1eeeade2383eca8f029f8752
Babita Bhatt <babitabhatta37@gmail.com>
2023-08-19 13:38:07 +0545

    ........

---
+++ recursion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    system("cls");
+    int x;
+    char cat[10]={"pradip bhatt"};
+    printf("Enter your name =");
+    scanf("%c",&cat);
+    printf("My name is %c",&cat);
+}
